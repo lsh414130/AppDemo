a8e4626126e9b0c216491f6358944177caa9bbec
刘书豪 <>
2018-07-16 11:36:39 +0800

    提交测试

---
+++ src/main/java/com/example/jeremylau/myapplication/activity/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.jeremylau.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jeremylau on 2018/7/16.
+ */
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
